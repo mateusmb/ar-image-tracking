a8d5152955e443c7fa72bd560773826b12f3d32d
mateusmb <mateusmbdev@gmail.com>
2022-06-08 20:12:25 -0300

    setup boilerplate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "mind-ar/dist/mindar-image.prod.js";
+import "aframe";
+import "mind-ar/dist/mindar-image-aframe.prod.js";
+import "./App.css";
+import MindARViewer from "./components/MindArViewer";
 
 function App() {
+  const [started, setStarted] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Example React component with MindAR</h1>
+
+      <div>
+        {!started && (
+          <button
+            onClick={() => {
+              setStarted(true);
+            }}
+          >
+            Start
+          </button>
+        )}
+        {started && (
+          <button
+            onClick={() => {
+              setStarted(false);
+            }}
+          >
+            Stop
+          </button>
+        )}
+      </div>
+
+      {started && (
+        <div className="container">
+          <MindARViewer />
+          <video></video>
+        </div>
+      )}
     </div>
   );
 }
